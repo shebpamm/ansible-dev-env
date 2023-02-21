9136035893f396052541b7c342222070beb55161
Erik Karsten <erik.karsten@eficode.com>
2023-02-21 12:54:06 +0200

    prune everything not related to just base ansible+mitogen

--- devenv.nix
+++ devenv.nix
@@ -4,33 +4,9 @@ let
   patched-pkgs = pkgs.extend (import ./packages.nix);
 in
 {
-  devcontainer.enable = true;
   languages.python.enable = true;
-  languages.python.package = pkgs.python310.withPackages (ps: with ps;
-    [
-      requests
-    ]
-  );
   packages = with patched-pkgs; [
     (wrapMitogen ansible_2_9)
-    python310Packages.hvac
-    python310Packages.pyvmomi
-    python310Packages.distro
-    ansible-modules-hashivault
     ansible-lint
-    zabbix-api
-    py-zabbix
-    cdrkit
-    sshpass
-    cowsay
-    lolcat
-    awscli
-    terraform
-    tfsec
-    vault-bin
-    azure-cli
-    azure-storage-azcopy
-    infracost
   ];
-  env = { };
 }
--- packages.nix
+++ packages.nix
@@ -51,48 +51,7 @@ in
       };
     };
 
-  zabbix-api = with python-pkgs;
-    buildPythonPackage rec {
-      pname = "zabbix-api";
-      version = "0.5.5";
-      src = fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-fnuEeSCQvTqlRvzLDhPIMNv+BDEwNQmwCfbSeDyiQY0=";
-      };
-    };
-
-  py-zabbix = with python-pkgs; buildPythonPackage {
-    pname = "py-zabbix";
-    version = "1.7.7";
-    format = "wheel";
-    src = pkgs.fetchurl {
-      url = "https://files.pythonhosted.org/packages/22/27/b75d1433caf55d077efe0dfadf494a53000bfc4ea5a1c9f197a87a3cd428/py_zabbix-1.1.7-py3-none-any.whl";
-      sha256 = "sha256-+SGryIKYxW9aq5BUgVEiypWfhhLfiP3DokCtLZXkwoI=";
-    };
-  };
-
-  ansible-modules-hashivault = with python-pkgs;
-    buildPythonPackage rec {
-      pname = "ansible-modules-hashivault";
-      version = "1.1";
-      src = pkgs.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-wywxV9+zQAiwhXa/gtMkZcc2dhJY1NP9plEL0KXq7lo=";
-      };
-    };
-
   ansible-lint = super.python310Packages.ansible-lint.overridePythonAttrs (old: {
     makeWrapperArgs = [ "--unset PYTHONPATH" ];
-  });
-
-  mitogen = super.python310Packages.mitogen.overridePythonAttrs (old: rec {
-    pname = "mitogen";
-    version = "0.2.10";
-    src = super.fetchFromGitHub {
-      owner = "mitogen-hq";
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "sha256-SFwMgK1IKLwJS8k8w/N0A/+zMmBj9EN6m/58W/e7F4Q=";
-    };
   });
 }
